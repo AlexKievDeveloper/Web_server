c6733756317f9573f042e7f18174997cfa5e570e
AlexKievDeveloper <glushkov18@gmail.com>
2020-07-01 20:47:44 +0300

    Added first classes of WebServer
    1) Added class Server, Client, ServerStarter.
    2) Added methods for work with client requests for html and css files.
    3) Added pom.xml and gitignore.
    4) Resources added (index.html, style.css).

---
+++ src/main/java/Server.java
@@ -0,0 +1,76 @@
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class Server {
+    private static final String LINE_END = "\n";
+    private String pathToResources;
+    private int port;
+
+    public void start() {
+        try (ServerSocket serverSocket = new ServerSocket(port);) {
+
+            while (true) {
+                try (Socket socket = serverSocket.accept();
+                     BufferedWriter socketWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+                     BufferedReader socketReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));) {
+
+                    while (true) {
+                        String lineFromClient = socketReader.readLine();
+                        if (lineFromClient.isEmpty()) {
+                            break;
+                        } else if (lineFromClient.contains("GET")) {
+                            String request = lineFromClient.substring(lineFromClient.indexOf('T') + 2, lineFromClient.indexOf('H') - 1);
+
+
+                            if (new File(pathToResources + request).exists() && request.length() > 1) {
+                                String result = getResourcesFromPath(pathToResources + request);
+                                socketWriter.write("HTTP/1.1 200 OK");
+                                socketWriter.write(LINE_END);
+                                socketWriter.write(LINE_END);
+                                socketWriter.write(result);
+                                socketWriter.flush();
+                            } else {
+                                socketWriter.write("HTTP/1.1 404 Not Found");
+                                socketWriter.write(LINE_END);
+                                socketWriter.write(LINE_END);
+                                socketWriter.flush();
+                            }
+                        }
+                    }
+                } catch (IOException ioException) {
+                    ioException.printStackTrace();
+                }
+            }
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
+        }
+    }
+
+    private String getResourcesFromPath(String request) {
+        try {
+            return new String(Files.readAllBytes(Paths.get(request)));
+        } catch (IOException ioException) {
+            throw new RuntimeException(ioException);
+        }
+    }
+
+    public String getPathToResources() {
+        return pathToResources;
+    }
+
+    public void setPathToResources(String pathToResources) {
+        this.pathToResources = pathToResources;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public void setPort(int port) {
+        this.port = port;
+    }
+}
+
---
+++ src/main/java/ServerStarter.java
@@ -0,0 +1,11 @@
+import java.io.IOException;
+
+public class ServerStarter {
+    public static void main(String[] args) throws IOException {
+        Server server = new Server();
+        server.setPort(3000);
+        server.setPathToResources("src/main/resources/webapp");
+        server.start();
+    }
+}
+
